cc2ed0fdcbbec085dd147ac0e638d0cd4634808e
Saharat Nasahachart <earthinwlo7@gmail.com>
2018-10-26 16:12:28 -0700

    Added ContactUsScreen GameObject and Script Changed Button references from Unity itself to C#

--- Unity/Assets/Scripts/ContactUsScreen.cs
+++ Unity/Assets/Scripts/ContactUsScreen.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SettingsScreen : MonoBehaviour {
-
+public class ContactUsScreen : MonoBehaviour
+{
     [SerializeField]
     private Button backButton;
+
+    [SerializeField]
+    private Button facebookButton;
 
     private void Start()
     {
         backButton.onClick.AddListener(CloseScreen);
+        facebookButton.onClick.AddListener(OpenFacebook);
     }
 
     private void OnDestroy()
     {
         backButton.onClick.RemoveListener(CloseScreen);
+        facebookButton.onClick.RemoveListener(OpenFacebook);
     }
 
     private void CloseScreen()
+    {
+        ScreenManager.Instance().PopScreen();
+    }
+
+    private void OpenFacebook()
     {
         ScreenManager.Instance().PopScreen();
     }
--- Unity/Assets/Scripts/LandingScreen.cs
+++ Unity/Assets/Scripts/LandingScreen.cs
@@ -9,14 +9,29 @@ public class LandingScreen : MonoBehaviour
     [SerializeField]
     private Button startGameButton;
 
+    [SerializeField]
+    private Button settingGameButton;
+
+    [SerializeField]
+    private Button contactGameButton;
+
+    [SerializeField]
+    private Button achievementGameButton;
+
     private void Start()
     {
         startGameButton.onClick.AddListener(StartButtonClickedCallback);
+        settingGameButton.onClick.AddListener(OpenSettingsScreen);
+        contactGameButton.onClick.AddListener(OpenContactScreen);
+        achievementGameButton.onClick.AddListener(OpenAchievementScreen);
     }
 
     private void OnDestroy()
     {
         startGameButton.onClick.RemoveListener(StartButtonClickedCallback);
+        settingGameButton.onClick.RemoveListener(OpenSettingsScreen);
+        contactGameButton.onClick.RemoveListener(OpenContactScreen);
+        achievementGameButton.onClick.RemoveListener(OpenAchievementScreen);
     }
 
     private void StartButtonClickedCallback()
@@ -24,7 +39,17 @@ public class LandingScreen : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
     }
 
-    public void OpenSettingsScreen()
+    private void OpenSettingsScreen()
+    {
+        ScreenManager.Instance().PushScreen("SettingsScreen");
+    }
+
+    private void OpenContactScreen()
+    {
+        ScreenManager.Instance().PushScreen("ContactUsScreen");
+    }
+
+    private void OpenAchievementScreen()
     {
         ScreenManager.Instance().PushScreen("SettingsScreen");
     }
